f6de558d48eb4435f3b0b172308fc867e5dfdbc0
ProsperAdeola01~ <adebiyiprosper7@gmail.com>
2023-10-17 20:09:06 +0100

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,42 +2,46 @@ import style from "./navbar.module.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import {Link} from "react-router-dom"
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 const Navbar1 = () => {
   return (
     <div className={style.navbar}>
-
-<Navbar className={style.nav} expand="lg" >
-        <Container className={style.Container}>
-          <img className={style.logo}src="./images/logo.png" alt="" />
-          <Nav className={`  ${style.ul}`}  >
-            <Link to='/' className={style.li} style={{color:"white"}}>Home</Link>
-            <Link to='/service' className={style.li} style={{color:"white"}}>Service</Link>
-            <Link to='/about'className={style.li} style={{color:"white"}}>About us</Link>
-            <Link to='/contact'className={style.li} style={{color:"white"}}>Contact us</Link>
-          </Nav>
+      <Navbar className={style.nav } expand="lg">
+        <Container>
+          <img className={style.logo} src="./images/lg.png" alt="" />
+          <Navbar.Toggle
+            className={style.toggle}
+            aria-controls="basic-navbar-nav"
+          />
+          <Navbar.Collapse className={style.collapse}>
+            <Nav className={`  ${style.ul}`}>
+              <Link to="/" className={style.li} style={{ color: "white" }}>
+                Home
+              </Link>
+              <Link
+                to="/service"
+                className={style.li}
+                style={{ color: "white" }}
+              >
+                Service
+              </Link>
+              <Link to="/about" className={style.li} style={{ color: "white" }}>
+                About us
+              </Link>
+              <Link
+                to="/contact"
+                className={style.li}
+                style={{ color: "white" }}
+              >
+                Contact us
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-
-
-
-      <Navbar expand="lg" className={"bg-body-tertiary"}>
-      <Container>
-      <img className={style.logo}src="./images/logo.png" alt="" />
-        <Navbar.Toggle className={style.toggle} aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className={style.collapse}>
-          <Nav className={`  ${style.ul}`} >
-          <Link to='/' className={style.li} style={{color:"white"}}>Home</Link>
-          <Link to='/service' className={style.li} style={{color:"white"}}>Service</Link>
-          <Link to='/about'className={style.li} style={{color:"white"}}>About us</Link>
-          <Link to='/contact'className={style.li} style={{color:"white"}}>Contact us</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </div>
+    </div>
   );
 };
 
