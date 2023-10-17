adc11fbefac738a63d57e8a8aec0fb70608c4175
ProsperAdeola01~ <adebiyiprosper7@gmail.com>
2023-10-17 19:47:49 +0100

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import style from "./navbar.module.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Link} from "react-router-dom"
 
 const Navbar1 = () => {
@@ -19,6 +20,23 @@ const Navbar1 = () => {
           </Nav>
         </Container>
       </Navbar>
+
+
+
+      <Navbar expand="lg" className={"bg-body-tertiary"}>
+      <Container>
+      <img className={style.logo}src="./images/logo.png" alt="" />
+        <Navbar.Toggle className={style.toggle} aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className={style.collapse}>
+          <Nav className={`  ${style.ul}`} >
+          <Link to='/' className={style.li} style={{color:"white"}}>Home</Link>
+          <Link to='/service' className={style.li} style={{color:"white"}}>Service</Link>
+          <Link to='/about'className={style.li} style={{color:"white"}}>About us</Link>
+          <Link to='/contact'className={style.li} style={{color:"white"}}>Contact us</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   </div>
   );
 };
